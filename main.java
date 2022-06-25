71e7fa44e1e63ee9ce6ad945a5e1d9656bdba8fa
hardi jadwani <hardirj185@gmail.com>
2022-06-25 07:01:14 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,14 @@
+
+package task_ATM;
+
+import java.io.IOException;
+
+
+public class main extends menu {
+     public static void main(String[] args) throws IOException
+    {
+        menu m=new menu();
+        m.GetLogin();
+    }
+    
+}
